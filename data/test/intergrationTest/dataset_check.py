805eab103660ad1cea25d21b0848c14280014791
Ashley Neaves <ashley.neaves@stfc.ac.uk>
2020-11-24 12:55:13 +0000

    Modified python check to calculate what SHOULD occur with the addition plugin, so its no longer hard coded

--- data/test/intergrationTest/dataset_check.py
+++ data/test/intergrationTest/dataset_check.py
@@ -21,13 +21,42 @@ class DatasetChecker:
 
     def check_averages(self):
         # this is very hard coded to the specific pattern used, work out some way to generalise it
-        frame = self.data[0]
+        processed_frame = self.data[0]
         raw_frame = self.raw[0]
-        assert_equal(frame[3, 3], 11)
-        assert_equal(frame[35, 30], 0)
-        assert_equal(frame[35, 31], 44)
-        assert_equal(frame[3, 31], 22)
-        assert_equal(raw_frame[35, 31], 11)
+        # go through raw_frame
+        # if raw pixel = 0, processed should also be 0
+        # if not 0, then need to check surrounding pixels for values
+        #   if no surrouding values, processed = raw
+        # suppose we could just generate what we think the processed frame should be then compare
+
+        # pad with a single 0 on each axis, so that we can still look "around" every pixel
+        expected_frame = np.pad(raw_frame, 1)  # expected frame is now shaped (82, 82)
+        cols = expected_frame.shape[1]
+        rows = expected_frame.shape[0]
+        for i in range(rows):
+            for j in range(cols):
+                pixel = expected_frame[i, j]
+                if not pixel == 0:
+                    # list the pixels neighbours
+                    # this ends up being a 3 by 3 square array with the original pixel in the middle
+                    # print("Pixel Position: {}".format((i, j)))
+                    neighbours = expected_frame[i - 1:i + 2, j - 1:j + 2]
+                    neighbour_sum = np.sum(neighbours)
+                    max_pos = np.unravel_index(neighbours.argmax(), neighbours.shape)
+                    max_pos = tuple(np.subtract(max_pos, (1, 1)))
+                    actual_pos = tuple(np.add((i, j), max_pos))
+
+                    # set all neighbours to 0
+                    expected_frame[i - 1:i + 2, j - 1:j + 2] = 0
+                    # set pixel @ max_pos to the sum
+                    expected_frame[actual_pos[0], actual_pos[1]] = neighbour_sum
+                    # print(expected_frame[i - 1:i + 2, j - 1:j + 2])
+
+        # trim the padding back off now that processing is complete
+        expected_frame = expected_frame[1:rows-1, 1:cols-1]
+        # we now have what the processed frame SHOULD look like when using Addition Plugin
+        assert_equal(processed_frame.shape, expected_frame.shape)
+        assert_true(np.array_equal(processed_frame, expected_frame))
 
 
 if __name__ == "__main__":
