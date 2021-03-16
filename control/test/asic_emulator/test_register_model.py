cb51b39c28bba905feb1ba8425cea8a5416de2ea
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2021-03-16 15:24:52 +0000

    Add further test cases for register model

--- control/test/asic_emulator/test_register_model.py
+++ control/test/asic_emulator/test_register_model.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from unittest.mock import Mock
 
@@ -7,7 +9,7 @@ from mercury.asic_emulator.register_model import MercuryAsicRegisterModel, Regis
 def test_register_model():
     """Test fixture for MercuryAsicRegisterModel tests"""
     emulator = Mock()
-    log_register_writes = False
+    log_register_writes = True
     register_model = MercuryAsicRegisterModel(emulator, log_register_writes)
     yield register_model
 
@@ -35,3 +37,66 @@ class TestRegisterModel():
         registers = test_register_model.registers()
         assert type(registers) is list
         assert len(registers) == RegisterMap.size()
+
+    def test_registers_have_default_values(self, test_register_model):
+
+        assert any(test_register_model.registers())
+
+    @pytest.mark.parametrize("transaction, result",
+        [
+            ([0x0,  0x0],  True),
+            ([0x80, 0x0], False),
+            ([0x1,  0x0],  True),
+            ([0xF3, 0x0], False),
+        ]
+    )
+    def test_is_write_transaction(self, test_register_model, transaction, result):
+
+        assert test_register_model.is_write_transaction(transaction) == result
+
+    @pytest.mark.parametrize("page_select, addr, result",
+        [
+            (0, 0x00, 0x00),
+            (0, 0x10, 0x10),
+            (1, 0x00, 0x00),
+            (1, 0x02, 0x02),
+            (1, 0x10, 0x90),
+        ]
+    )
+    def test_calc_register_addr(self, test_register_model, monkeypatch, page_select, addr, result):
+
+        monkeypatch.setattr(test_register_model, "page_select", page_select)
+        assert test_register_model.calc_register_addr(addr) == result
+
+    def test_write_transaction(self, test_register_model):
+
+        transaction = [0x0, 0x1, 0x2, 0x3]
+        response = test_register_model.process_transaction(transaction)
+
+        assert response == transaction
+        assert test_register_model.registers()[0:3] == transaction[1:]
+
+    def test_read_transaction(self, test_register_model):
+
+        reg_vals = [0x0, 0xde, 0xad, 0xbe, 0xef]
+        transaction = [0x0] + reg_vals
+        response = test_register_model.process_transaction(transaction)
+
+        transaction = [0x80] + [0x0]*len(reg_vals)
+        response = test_register_model.process_transaction(transaction)
+        assert response == [0x80] + reg_vals
+
+    def test_log_register_writes(self, test_register_model, caplog):
+
+        with caplog.at_level(logging.DEBUG):
+            value = 0x1
+            transaction = [RegisterMap.CONFIG1, value]
+            test_register_model.process_transaction(transaction)
+            assert f"CONFIG1 register: {value:#04x}" in caplog.text
+
+    def test_transaction_error(self, test_register_model, caplog):
+
+        caplog.clear()
+        transaction = 0
+        test_register_model.process_transaction(transaction)
+        assert "Error processing transaction" in caplog.text
